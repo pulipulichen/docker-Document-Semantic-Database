15171aff80e2fca0a2c64175a794994e5790ef8d
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-03-06 13:36:17 +0800

    西元2025年03月06日 (週四) 13時36分17秒 CST

--- unstructured/test/table.js
+++ unstructured/test/table.js
@@ -5,7 +5,8 @@ const FormData = require('form-data');
 // const FILE = 'table_1.pdf'
 // const FILE = 'Year02.xls'
 // const FILE = 'Year02.xls'
-const FILE = 'table.pdf'
+// const FILE = 'table.pdf'
+const FILE = 'table_p1.pdf'
 
 async function uploadFile() {
     try {
