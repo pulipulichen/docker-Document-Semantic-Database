bafc91776707c8d919807b46906941d49f4bfce8
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-03-07 22:06:02 +0800

    西元2025年03月07日 (週五) 22時06分02秒 CST

--- unstructured/test/table_pdf.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -X POST http://localhost:8080/process -F "file=@table_1.pdf"
-# curl -X POST http://localhost:8080/process -F "file=@table.pdf"
--- unstructured/test/xls.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -X POST http://localhost:8080/process -F "file=@Year02.xls"
-# curl -X POST http://localhost:8080/process -F "file=@table.pdf"
